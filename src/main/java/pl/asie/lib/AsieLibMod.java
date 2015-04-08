6d4fa2c84b46844660904e4d470325c24ae2a8fb
Vexatos <Stuarzt@gmx.de>
2015-04-08 10:34:38 +0200

    This is not needed anymore.

--- src/main/java/pl/asie/lib/AsieLibMod.java
+++ src/main/java/pl/asie/lib/AsieLibMod.java
@@ -28,7 +28,7 @@ import pl.asie.lib.tweak.enchantment.EnchantmentTweak;
 
 import java.util.Random;
 
-@Mod(modid = Mods.AsieLib, name = Mods.AsieLib_NAME, version = "@VERSION@", useMetadata = true,
+@Mod(modid = Mods.AsieLib, name = Mods.AsieLib_NAME, version = "@VERSION@",
 	dependencies = "required-after:Forge@[10.13.2.1236,);after:gregtech@[MC1710];"
 		+ "after:CoFHAPI|block@[1.7.10R1.0.0,);after:CoFHAPI|energy@[1.7.10R1.0.0,);"
 		+ "after:CoFHAPI|tileentity@[1.7.10R1.0.0,);after:CoFHAPI|item@[1.7.10R1.0.0,)")
